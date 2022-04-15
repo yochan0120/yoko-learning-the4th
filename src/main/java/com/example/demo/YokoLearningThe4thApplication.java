84ee4772ede7f2954b38a4fbe29d44fe5828b048
yoko morioka <yoko@MacBook-Pro.local>
2022-04-15 09:24:53 +0900

    4回目の課題

---
+++ src/main/java/com/example/demo/YokoLearningThe4thApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YokoLearningThe4thApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YokoLearningThe4thApplication.class, args);
+	}
+
+}
